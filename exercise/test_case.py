34c781b7624a451e2264933aaa63f48f8978a948
Taeoh <kimtaeoh95@gmail.com>
2018-07-13 02:16:54 +0900

    error fix

--- exercise/test_case.py
+++ exercise/test_case.py
@@ -1,26 +1,30 @@
 import pytest
+import os
 from readInputTextFile import readInputTextFile
 from deleteOutlier import deleteOutlier
 from remainOddOrEven import remainOddOrEven
 from countNumOfUnique import countNumOfUnique
 from myOwnCounter import myOwnCounter
 
+dirname = os.path.dirname(os.path.realpath(__file__))
+filename = os.path.join(dirname, "input_ex.txt")
+
 def test_readInputTextFile():
-    func_list = readInputTextFile("input_ex.txt")
-    answer_list = [6, 14, 9, 2, 25, 1, 15, 7, 13, 11, 7, 16, 26, 19, 12, 15, 14]
+    func_list = readInputTextFile(filename)
+    answer_list = [6, 13, 9, 2, 25, 1, 15, 7, 13, 11, 7, 16, 26, 19, 12, 15, 14]
 
     assert func_list == answer_list
 
 
 def test_deleteOutlier():
-    input_list = [6, 14, 9, 2, 25, 1, 15, 7, 13, 11, 7, 16, 26, 19, 12, 15, 14]
+    input_list = [6, 13, 9, 2, 25, 1, 15, 7, 13, 11, 7, 16, 26, 19, 12, 15, 14]
     func_list = deleteOutlier(input_list)
-    answer_list = [14, 15, 13, 11, 16, 19, 12, 15, 14]
+    answer_list = [13, 15, 13, 11, 16, 19, 12, 15, 14]
 
     assert func_list == answer_list
 
 def test_remainOddOrEven():
-    input_list = [14, 15, 13, 11, 16, 19, 12, 15, 14]
+    input_list = [13, 15, 13, 11, 16, 19, 12, 15, 14]
     func_list = remainOddOrEven(input_list)
     answer_list = [13, 15, 13, 11, 19, 15]
 
@@ -33,6 +37,6 @@ def test_countNumOfUnique():
     assert func == 4
 
 def test_myOwnCounter():
-    func = myOwnCounter("input_ex.txt")
+    func = myOwnCounter(filename)
 
     assert func == 4
